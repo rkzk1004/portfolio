06e9c881f3a28d38b5ef7aff026765d88593e1e9
rkzk1004 <rkzk1004@naver.com>
2025-05-09 11:35:40 +0900

    feature영역 gasp 테스트 완료

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,5 +1,33 @@
-$(function () {
-  // 마우스 따라다니는 동그라미
+// gsap 스크롤트리거 등록
+gsap.registerPlugin(ScrollTrigger);
+
+// 모든 해상도
+function initCommon() {
+  // sc-feature 애니메이션
+  $('.sc-feature .group-list').each(function () {
+    gsap.from(this, {
+      scrollTrigger: {
+        // markers: true,
+        trigger: this,
+        start: "0 85%",
+        end: "0 0"
+      },
+      y: 100,
+      opacity: 0,
+      duration: 2,
+      ease: "power4.out"
+    })
+  })
+
+  // 푸터 - back to top 클릭하면 맨 위로 이동
+  $('.btn-top').on('click', function () {
+    $('html,body').animate({ scrollTop: 0 }, 400)
+  })
+}
+
+// PC 전용
+function initPC() {
+  // 공통 - 마우스 따라다니는 동그라미
   const $cursorEl = $('.deco-mouse');
   $(document).on('mousemove', function (e) {
     const mouseX = e.clientX;
@@ -33,7 +61,7 @@ $(function () {
     $cursorEl.removeClass('contact-in')
   })
 
-  // 공통 마그네틱 효과
+  // 공통 - 마그네틱 효과
   const movement = 15; // 이동범위
   $('.ef-magnatic').each(function () {
     const $magnaticEl = $(this);
@@ -60,5 +88,27 @@ $(function () {
         ease: "power2.out"
       });
     });
+  });
+}
+
+// 모바일 전용
+function initMobile() {
+}
+
+// 해상도에 따라 스크립트 실행
+function responsiveScripts() {
+  initCommon();
+  if ($(window).width() >= 1025) {
+    initPC();
+  } else {
+    initMobile();
+  }
+}
+
+// 문서 준비 완료 시 실행
+$(document).ready(function () {
+  responsiveScripts();
+  $(window).resize(function () {
+    responsiveScripts();
   });
 });
