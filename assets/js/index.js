ec115ac8b3c0e14b71c316b792f902b96ec49289
rkzk1004 <rkzk1004@naver.com>
2025-05-13 11:29:58 +0900

    test01

--- assets/js/index.js
+++ assets/js/index.js
@@ -76,7 +76,7 @@ function initCommon() {
   // sc-main 애니메이션 - gruop-text
   gsap.to(".sc-main h2 .group-text", {
     scrollTrigger: {
-      // markers: true,
+      markers: true,
       trigger: ".sc-main",
       start: "0 0",
       end: "50% 0",
